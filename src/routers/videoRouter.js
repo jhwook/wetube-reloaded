ce047e19494464a6b6e644a5eb8d2a9dbe3847ba
jhwook <sprtzk@naver.com>
2022-01-19 10:54:04 +0900

    1/19

---
+++ src/routers/videoRouter.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+
+const videoRouter = express.Router();
+
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id/edit(\\d+)", edit);
+videoRouter.get("/:id/delete(\\d+)", deleteVideo);
+
+export default videoRouter;
